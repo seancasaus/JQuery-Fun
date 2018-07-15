c2540bbaa35e8edd3f469c367effe93612ac9f57
Sean <shawn.casaus@gmail.com>
2018-07-14 19:16:10 -0700

    had fun with events and effects

---
+++ Events-and-Effects/js/clickEvent.js
@@ -0,0 +1,12 @@
+$("#box").show();
+$("#target").on("click", function () {
+  //$("p#statement").hide();
+  //$("#box").toggle();
+  //$("#box").slideDown(1000);
+  //$("#box").slideUp(1000);
+  //$("#box").slideToggle(1000);
+  //$("#box").fadeIn(1000);
+  //$("#box").fadeOut(1000);
+  //$("#box").fadeToggle(1000, 0.5);
+  $("#box").fadeTo(1000, 0.5);
+});
